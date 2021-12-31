9f76dbe8652b3ce5f0aa286cc7df9d751dfec1bc
Shafeer <shafeer.ali@gmail.com>
2021-12-31 11:46:43 +0530

    Add project files.

---
+++ MyFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly DaprClient _daprClient;
+
+        public IndexModel(DaprClient daprClient, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            var forecasts = await _daprClient.InvokeMethodGrpcAsync<Google.Protobuf.IMessage>("mybackend", "weatherforecast");
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
