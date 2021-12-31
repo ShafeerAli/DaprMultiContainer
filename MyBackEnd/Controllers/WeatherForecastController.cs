d1be5ad5a2b914cb6bc9c60259cfe8b7ae0b61cc
Shafeer <shafeer.ali@gmail.com>
2021-12-31 12:34:15 +0530

    Code changes

--- MyBackEnd/Controllers/WeatherForecastController.cs
+++ MyBackEnd/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace MyBackEnd.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = "No Summary"//Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
--- MyFrontEnd/Pages/Index.cshtml.cs
+++ MyFrontEnd/Pages/Index.cshtml.cs
@@ -17,7 +17,7 @@ namespace MyFrontEnd.Pages
 
         public async Task OnGet()
         {
-            var forecasts = await _daprClient.InvokeMethodGrpcAsync<Google.Protobuf.IMessage>("mybackend", "weatherforecast");
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(HttpMethod.Get,"mybackend", "weatherforecast");
             ViewData["WeatherForecastData"] = forecasts;
         }
     }
